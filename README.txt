6031354b841e46133374ea15f93e18234a859c06
Andrew Leahey <andrew@leahey.org>
2022-12-17 08:11:11 -0500

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,7 @@
+README
+
+Plugin Name: Mastodon Post Display
+Description: Display your Mastodon posts on a WordPress page
+
+
+To use this plugin, simply add the [mastodon-post-display] shortcode to any WordPress page where you want to display your Mastodon posts.
---
+++ WP-GetMastodon.php
@@ -0,0 +1,65 @@
+<?php
+/*
+Plugin Name: Mastodon Post Display
+Description: Display your Mastodon posts on a WordPress page
+*/
+
+function mastodon_post_display_shortcode() {
+  // Get your Mastodon posts
+  $posts = get_mastodon_posts();
+
+  // Check if there are any posts to display
+  if (empty($posts)) {
+    return 'No Mastodon posts to display.';
+  }
+
+  // Create a container element for the posts
+  $html = '<div id="mastodon-post-display">';
+
+  // Loop through the posts and create a list of post elements
+  foreach ($posts as $post) {
+    $html .= '<div class="mastodon-post">';
+
+    // Add the post title in bold
+    $html .= '<h3><strong>' . $post['title'] . '</strong></h3>';
+
+    // Add the post content
+    $html .= '<p>' . $post['content'] . '</p>';
+
+    // Add the post publication date
+    $html .= '<p><em>' . $post['pub_date'] . '</em></p>';
+
+    $html .= '</div>';
+  }
+
+  // Close the container element
+  $html .= '</div>';
+
+  // Return the final HTML
+  return $html;
+}
+add_shortcode('mastodon-post-display', 'mastodon_post_display_shortcode');
+
+function get_mastodon_posts() {
+  // Get the RSS feed content
+  $rss = file_get_contents('https://esq.social/@andrew.rss');
+
+  // Load the RSS feed into a SimpleXML object
+  $xml = simplexml_load_string($rss);
+
+  // Initialize an empty array to store the posts
+  $posts = [];
+
+  // Loop through the items in the RSS feed
+  foreach ($xml->channel->item as $item) {
+    // Add the post to the array
+    $posts[] = [
+      'title' => (string) $item->title,
+      'content' => (string) $item->description,
+      'pub_date' => (string) $item->pubDate,
+    ];
+  }
+
+  // Return the array of posts
+  return $posts;
+}
